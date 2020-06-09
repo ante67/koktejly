4e9a359bf8325821299b9364caa56721363556e0
ante67 <66181513+ante67@users.noreply.github.com>
2020-06-10 00:06:53 +0200

    4. commit

--- src/main/java/cz/amware/koktejly/app/controller/IngredientController.java
+++ src/main/java/cz/amware/koktejly/app/controller/IngredientController.java
@@ -12,6 +12,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import cz.amware.koktejly.app.data.IIngredientData;
+import cz.amware.koktejly.app.model.IngredientsFormModel;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 /**
  *
@@ -28,15 +31,14 @@ public class IngredientController {
     private IIngredientData ingredientData; 
     
     @GetMapping("/") /*local url  http://localhost:8080/ingredients */
-   // @RequestMapping("/ingredients")
-        
     public String index(Model model){            
-        //model.addAttribute("welcome1", welcome1);
-        //model.addAttribute("message", "kunda");
-        //model.
+        model.addAttribute("ingredientsFormModel", new IngredientsFormModel());
         model.addAttribute("ingredients",ingredientData.findAll());         
         return "index";  /* vrátím html z Web Pages        */      
     }
-    
-    
+    @PostMapping("/result") /*local url  http://localhost:8080/ingredients */
+    public  String result(@ModelAttribute IngredientsFormModel formModel,Model model){
+        Long[] ingredientIds = formModel.getIngredientIds(); //
+        return "result";        
+    }            
 }
